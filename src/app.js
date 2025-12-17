5d887bc48b9bf9cf5072f298b689338c59fef075
mouad <hamideddinemouad@gmail.com>
2025-12-17 04:22:47 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import registerRoutes from "./routes/auth/registerRoutes.js";
 import adminRoutes from "./routes/adminroutes/adminroutes.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 await mongoose.connect("mongodb://localhost:27017/suivi-trajets");
 
 const PORT = 3000;
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth/login", loginRoutes);
@@ -17,7 +19,7 @@ app.use("/api/auth/register", registerRoutes);
 app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, ()=>{
-    console.log("lisening on port " + PORT);
+    console.log("http://localhost:" + PORT + "/");
 })
 
 
